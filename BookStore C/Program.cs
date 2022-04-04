f6105bc199390dd1beaefac190f3868f9ae4af4a
elvecho <100934190+elvecho@users.noreply.github.com>
2022-04-04 18:30:39 +0200

    finito senza bonus

--- BookStore C/Program.cs
+++ BookStore C/Program.cs
@@ -1,3 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
+string titolo = "Clean Code: A Handbook of Agile Software";
+string autore = "Robert C. Martin";
+long ISBN = 9780132350884;
+int pagine = 431;
+int peso = 860;
+double larghezza = 17.91;
+double profondità = 2.54;
+double altezza = 23.5;
+double valutazione = 4.7;
+int recensioni = 3311;
+bool kindle = true;
+bool CopertinaFlessibile = true;
+
+Console.WriteLine("il libro di oggi: " + titolo + " di " + autore);
+Console.WriteLine("ISBN: "+ ISBN);
+Console.WriteLine("numero delle pagine: " + pagine + " pagine");
+Console.WriteLine("peso del libro: "+ peso + "g");
+Console.WriteLine("Dimensioni del libro: " + larghezza + "cm X " + altezza + "cm X" + profondità + "cm");
+Console.WriteLine("Informazioni Amazon:");
+Console.WriteLine("Numero di recensioni: "+ recensioni + " recensioni");
+Console.WriteLine("Valutazione media: " + valutazione + " stelline");
+Console.WriteLine("Kindle disponibile: " + kindle);
+Console.WriteLine("Copertina flessibile disponibile: " + CopertinaFlessibile);
+
+
+
+
+
